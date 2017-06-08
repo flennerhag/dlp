b6a391ef722bafd0f3a49ec81c8d35176777aa41
Flennerhag <sebastianflennerhag@hotmail.com>
2017-06-08 11:33:03 +0200

    minor fix

--- deeplearn/viz.py
+++ deeplearn/viz.py
@@ -30,23 +30,23 @@ def plot_train_scores(trainer, burn_in=0, figsize=(12, 5), title=None):
     if title is not None:
         f.suptitle(title)
 
-    ax[0].set_title("Accuracy")
-    ax[0].plot(range(burn_in, stop, ival),
+    ax[0].set_title("Score")
+    ax[0].plot(range(start, stop, ival),
                train_score[int(burn_in/ival):],
                linewidth=0.5, color='k', label="train")
 
-    ax[0].plot(range(burn_in, stop, ival),
+    ax[0].plot(range(start, stop, ival),
                test_score[int(burn_in/ival):],
                linewidth=0.5, color='r', label="test")
 
     ax[0].legend(frameon=False)
 
     ax[1].set_title("Loss")
-    ax[1].plot(range(burn_in, stop), train_loss[burn_in:],
+    ax[1].plot(range(start, stop), train_loss[burn_in:],
                linewidth=0.5, color='k')
 
     ax[2].set_title("Gradient norm")
-    ax[2].plot(range(burn_in, stop), gradient_norm[burn_in:],
+    ax[2].plot(range(start, stop), gradient_norm[burn_in:],
                linewidth=0.5, color='k')
 
     return ax
